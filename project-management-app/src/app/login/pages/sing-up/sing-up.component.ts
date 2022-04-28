5d38bbeaaf8f7644a71d311bc07157214384bd51
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2022-04-28 20:17:16 +0300

    feat: sign-up layout

--- project-management-app/src/app/login/pages/sing-up/sing-up.component.ts
+++ project-management-app/src/app/login/pages/sing-up/sing-up.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sing-up',
   templateUrl: './sing-up.component.html',
   styleUrls: ['./sing-up.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SingUpComponent implements OnInit {
+export class SingUpComponent {
+  signUpData: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.signUpData = new FormGroup({
+      email: new FormControl('', []),
+      userName: new FormControl('', []),
+      password: new FormControl('', []),
+      repeatedPassword: new FormControl('', []),
+    });
   }
 
+  onSubmit() {}
 }
