f0fabe02ed01384510acf3de88ead4c913736ff8
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-04-30 19:20:46 +0300

    refactor: comb app.module

--- project-management-app/src/app/app.module.ts
+++ project-management-app/src/app/app.module.ts
@@ -1,33 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonToggleModule} from "@angular/material/button-toggle";
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
-import MainPageComponent from './core/pages/main-page/main-page.component';
-import ProjectsPageComponent from './core/pages/projects-page/projects-page.component';
-import HeaderComponent from './core/components/header/header.component';
-import FooterComponent from './core/components/footer/footer.component';
 import {CoreModule} from "./core/core.module";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent,
-    ProjectsPageComponent,
-    HeaderComponent,
-    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonToggleModule,
-    MatButtonModule,
-    MatIconModule,
     CoreModule,
   ],
   providers: [],
--- project-management-app/src/app/core/core.module.ts
+++ project-management-app/src/app/core/core.module.ts
@@ -4,19 +4,33 @@ import { RouterModule } from '@angular/router';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ApiInterceptor } from './interceptors/api.interceptor';
+import MainPageComponent from "./pages/main-page/main-page.component";
+import ProjectsPageComponent from "./pages/projects-page/projects-page.component";
+import HeaderComponent from "./components/header/header.component";
+import FooterComponent from "./components/footer/footer.component";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-
+    MainPageComponent,
+    ProjectsPageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   exports: [
-
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatButtonToggleModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true}
