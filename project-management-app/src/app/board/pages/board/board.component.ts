829fe7b61022efa46462de2e8b74a3c01ba106b4
klimentsii <klimparh@gmail.com>
2022-05-07 19:37:52 +0300

    create: drag-n-drop

--- project-management-app/src/app/board/pages/board/board.component.ts
+++ project-management-app/src/app/board/pages/board/board.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { Component, VERSION, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDropList } from '@angular/cdk/drag-drop';
+
+import { Board } from '../../models/board.model';
+import { Column } from '../../models/column.model';
 
 @Component({
   selector: 'app-board',
@@ -6,4 +11,38 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./board.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class BoardComponent {}
+export default class BoardComponent {
+
+  public board: Board = new Board('Test Board', [
+    new Column('Ideas', '21', [
+      'Some random idea',
+      'This is another random idea'
+    ]),
+    new Column('Research', '32', [
+      'Lorem ipsum',
+      'foo'
+    ])
+  ]);
+
+  constructor(){}
+
+  public ngOnInit(): void {
+    console.log(this.board);
+  }
+
+  public dropGrid(event: CdkDragDrop<string[]>): void {
+    moveItemInArray(this.board.columns, event.previousIndex, event.currentIndex);
+  }
+
+  public drop(event: CdkDragDrop<string[]>): void {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          event.currentIndex);
+    }
+  }
+
+}
